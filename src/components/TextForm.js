5fd3efe4581560189bceef297c7dc8d0114e7d09
Chaitali D <cudandale@gmail.com>
2022-05-14 01:25:27 -0400

    bug(css): Fixed a bug where we could use the function buttons even if there was no text in the text area, fixed it by disabling the buttons until some text is entered in the text area

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,76 +18,51 @@ export default function TextForm(props) {
 
     /**Function to convert the text in the text area to uppercase */
     const handleUpperClick = () => {
-        if(text.length>0){
-            let newText = text.toUpperCase();
-            setText(newText);
-            props.showAlert('Converted to Uppercase.','success');
-        }else{
-            props.showAlert('No text available for conversion.Please enter text to convert to uppercase','warning');
-        }
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('Converted to Uppercase.','success');
     }
 
     /**Function to remove the extra spaces in between the text so as to make the text more readable */
     const handleRemoveSpaceClick =() => {
-        if(text.length>0){
-            let newText = text.split(/[ ]+/);
-            setText(newText.join(' '));
-            props.showAlert('Removed lots of extraspaces from the text.','success'); 
-        }else{
-            props.showAlert('No text available.Please enter text to remove extra spaces..','warning');
-        }
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+        props.showAlert('Removed lots of extraspaces from the text.','success');     
     }
 
     /**Function to copy the entire text from the text area into clipboard  */
     const handleCopyClick = () =>{
-        if(text.length>0){
         let text = document.getElementById('exampleFormControlTextarea1');
         text.select();
         navigator.clipboard.writeText(text.value);
         props.showAlert('Text is copied to the clipboard.','success');
-        }else{
-            props.showAlert('Cannot copy empty text.Please enter text to be copied!','warning');
-        }
     }
 
     /**Function to clear the textarea */
     const handleClearClick = () =>{
-        if(text.length>0){
         let newText = '';
         setText(newText);
         props.showAlert('Text area is cleared.','success');
-        }else{
-            props.showAlert('Text area is already clear.','warning');
-        }
     }
 
     /**Fuction to read out the text that is currently present in the textarea */
     const handleSpeakClick = () => {
-        if(text.length>0){
         if(text){
             let msg = new SpeechSynthesisUtterance();
             msg.text = text;
             window.speechSynthesis.speak(msg);
-            props.showAlert('Reading the text...','success');  
-            
-        }
-        }else{
-            props.showAlert('Cannot read nothing.. :P. Please enter some text!','warning');
+            props.showAlert('Reading the text...','success');      
         }
     }
 
     /**Function to change the color in the text area randomly. This is fun functionality specific to a page */
     const handleColorClick = () => {
-        if(text.length>0){
-            const colors = ['sandybrown', 'mediumaquamarine', 'coral','indianred','dodgerblue','blueviolet','seagreen']
-            let randomcolor = Math.random()*10 ;
-            let color = (Math.ceil(randomcolor))%7;
-            document.querySelector('#exampleFormControlTextarea1').style.background = 'rgb(33,37,41, 10%)';
-            document.querySelector('#exampleFormControlTextarea1').style.color = colors[color];  
-            props.showAlert('Text color changed','success'); 
-        }else{
-            props.showAlert('Cannot change color of empty text.Please enter text to change color.','warning');
-        }         
+        const colors = ['sandybrown', 'mediumaquamarine', 'coral','indianred','dodgerblue','blueviolet','seagreen']
+        let randomcolor = Math.random()*10 ;
+        let color = (Math.ceil(randomcolor))%7;
+        document.querySelector('#exampleFormControlTextarea1').style.background = 'rgb(33,37,41, 10%)';
+        document.querySelector('#exampleFormControlTextarea1').style.color = colors[color];  
+        props.showAlert('Text color changed','success');            
     }
 
     /**Function to record any changes in the text area and use these changes to update the text variable 
@@ -111,20 +86,20 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea value={text} onChange ={handleOnChange} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
-                <button className="btn btn-success mx-2 my-3" onClick={handleLowerClick}>To Lowercase</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleUpperClick}>To Uppercase</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleRemoveSpaceClick}> Remove extra spaces</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleCopyClick}>Copy the text</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleClearClick}>Clear Text Area</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleSpeakClick}>Read the text</button>
-                <button className="btn btn-success mx-2 my-3" onClick={handleColorClick}>Change text color</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleLowerClick}>To Lowercase</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleUpperClick}>To Uppercase</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleRemoveSpaceClick}> Remove extra spaces</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleCopyClick}>Copy the text</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleClearClick}>Clear Text Area</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleSpeakClick}>Read the text</button>
+                <button disabled ={text.length === 0} className="btn btn-success mx-2 my-3" onClick={handleColorClick}>Change text color</button>
             </div>
             <div className="container my-3">
                 <h1>Your text summary:</h1>
                 <p>{(text ==='') ? 0 : text.split(' ').filter((element) =>{return element.length !=0}).length } words and {text.length} characters</p>
                 <p>Total read time will be { (text.length>0?(text.split(' ').length)/125 : 0)} minutes</p>
                 <h2>Preview:</h2>
-                <p>{text.length>0?text: 'Enter some  text above to check the preview . . .'}</p>
+                <p>{text.length>0?text: 'Nothing to preview . . .'}</p>
             </div>
         </>
     )
